6c9140304c5afcc87b7fac3538d879207da4c514
AugustineKadima <kadimasam@gmail.com>
2022-02-18 20:45:43 +0300

    Create employee class and employee test class

---
+++ Employee.py
@@ -0,0 +1,10 @@
+from re import A
+
+
+class Employee:
+    def __init__(self, first_name, last_name, email, phone_number, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.age = age
---
+++ Employee_Test.py
@@ -0,0 +1,6 @@
+import unittest
+import Employee
+
+class EmployeeTest(unittest.TestCase):
+    def test_employee_instance_creates_successfully(self):
+        employee_x = new Employee()

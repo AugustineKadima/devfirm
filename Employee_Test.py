345677ecf4c4d334542b5ba752940923b062a46d
AugustineKadima <kadimasam@gmail.com>
2022-02-18 22:28:19 +0300

    Add test co confirm that employee is saved successfully

--- Employee_Test.py
+++ Employee_Test.py
@@ -10,6 +10,14 @@ class EmployeeTest(unittest.TestCase):
         employee_x = Employee("John", "Doe", "johndoe@gmail.com", "07846563", 30)
         self.assertEqual(employee_x.fullname, "John Doe")
 
+    def test_employee_is_saved_successfully(self):
+        employee_x = Employee("John", "Doe", "johndoe@gmail.com", "07846563", 30)
+        employee_x.save()
+        employee_x = Employee("Kelvin", "Hart", "kelvinhart@gmail.com", "07854546", 70)
+        employee_x.save()
+        self.assertEqual(len(Employee.employee_list), 2)
+
+
 
 
 if __name__ == "__main__":
